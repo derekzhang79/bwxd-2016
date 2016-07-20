d9d5f725cd51aca4fecabf350ddf3d37efa987a6
Matt Cooper <matthew_cooper@brown.edu>
2016-07-19 23:54:29 -0400

    Updated slide out menu navigation and added new pages

--- Phase 2/Site/scripts/layout-index.js
+++ Phase 2/Site/scripts/layout-index.js
@@ -1,4 +1,8 @@
+var desktopWidth = 1025;
 $(window).load(function(loadEvent){
+    layout();
+});
+$(window).scroll(function(scrollEvent){
     layout();
 });
 $(window).resize(function(resizeEvent){
@@ -6,6 +10,12 @@ $(window).resize(function(resizeEvent){
 });
 
 function layout() {
-    var navbar = $('.navbarDiv').first();
+	var navbar = $('.navbarDiv').first();
+    if ($(window).width() < desktopWidth || $('body').scrollTop() > 0) {
+        navbar.css('border-bottom', '2px solid #00B5AD');
+    } else {
+        navbar.css('border-bottom', 'none');
+    }
+	
     $('#menu').css('margin-top', navbar.height());
 }
