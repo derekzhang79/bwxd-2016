d935e0f955978b8f69a093e9e0291ece7901e288
Matt Cooper <matthew_cooper@brown.edu>
2016-07-21 00:17:32 -0400

    Navigation updates

--- Phase 2/Site/scripts/layout.js
+++ Phase 2/Site/scripts/layout.js
@@ -1,4 +1,5 @@
 var desktopWidth = 1025;
+
 $(window).load(function(loadEvent){
     layout();
 });
@@ -26,4 +27,25 @@ function layout() {
     //set min content height to 100vh (minus navbar height) so that right border for menu 
     //goes all the way down on shorter pages (e.g. sponsors)
     $('.contentContainer').css('min-height', $(window).height() - navbar.height());  
+	
+	
+	// stick submenu to bottom of navbar as it scrolls down
+	if ($(window).width() >= desktopWidth) {
+		var submenu = $('.submenu').first();
+		var header = $('#header')
+		
+		headerBottom = header.offset().top + header.height()
+		navBottom = navbar.offset().top + navbar.height() + parseInt(navbar.css('border-bottom-width'))
+		
+		submenu.css('position', 'fixed')
+		if (navBottom >= headerBottom) {
+			submenu.css('top', navbar.height())
+		} else {
+			submenu.css('position', 'absolute')
+			submenu.css('top', headerBottom - navbar.height())
+		}
+	}
+	
+	
+	
 }
