b747e75f68f6a40b74afa77031f54ec039ccdcf2
Matt Cooper <matthew_cooper@brown.edu>
2016-08-10 21:16:54 -0400

    Started making changes in May's last email up to fixing arrows

--- Phase 2/Site/scripts/schedule.js
+++ Phase 2/Site/scripts/schedule.js
@@ -18,26 +18,38 @@ function layoutSchedule() {
 	wasDesktop = isDesktop;
 }
 
-$(".toggle").click(function () {
-	arrow = $(this).children('img');
-	content = $(this).next();
+$(".scheduleDayToggle").click(function () {
+	var arrow = $(this).children('img');
+	var content = $(this).parent().find('.toggleContent').first();
 
 	//rotate arrow and color title if it's an event toggle
 	if (content.is(':hidden')) {
 		arrow.css('transform', 'scale(1,-1)');
-
-//		if ($(this).hasClass('scheduleEventContentToggle')) {
-//			title = $(this).closest('.scheduleEvent').find('.scheduleEventTitle');
-//			console.log(title);
-//			title.css('color', '#FFD400');
-//		}
 	} else {
 		arrow.css('transform', 'scale(1,1)');
+	}
 
-//		if ($(this).hasClass('scheduleEventContentToggle')) {
-//			title = $(this).closest('.scheduleEvent').find('.scheduleEventTitle');
-//			title.css('color', '#241F59');
-//		}
+	//toggle content visibility
+	content.slideToggle(500);
+});
+
+$(".scheduleEventContentToggle").click(function () {
+	var arrow = $(this).children('svg');
+	var arrowPoly = arrow[0].getElementsByClassName('arrowPoly')[0];
+	
+	var title = $(this).children('h1');
+	var content = $(this).parent().find('.toggleContent').first();
+
+	//rotate arrow and color title if it's an event toggle
+	if (content.is(':hidden')) {
+		title.css('color', '#FFD400');
+		arrow.css('transform', 'scale(1,-1)');
+		arrowPoly.style.fill = 'FFD400';
+		
+	} else {
+		title.css('color', '#241F59');
+		arrow.css('transform', 'scale(1,1)');
+		arrowPoly.style.fill = '#241F59';
 	}
 
 	//toggle content visibility
