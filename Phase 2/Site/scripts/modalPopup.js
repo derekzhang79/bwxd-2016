9908907800bc44ad17a3a29e38d935b279b8f051
Matt Cooper <matthew_cooper@brown.edu>
2016-09-03 19:23:37 -0400

    More modal work

--- Phase 2/Site/scripts/modalPopup.js
+++ Phase 2/Site/scripts/modalPopup.js
@@ -1,2 +1,17 @@
-$("a[rel*=leanModal]").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+var offsetTop = $(window).height() * 0.1;
 
+$("a[rel*=leanModal]").leanModal({ top : offsetTop, overlay : 0.4, closeButton: ".modal_close" });
+console.log("A");
+
+a = 'a';
+
+function populateModal(elt) {
+	console.log(elt);
+	var name = elt.parent('.personProfile').attr('id');
+	console.log(name);
+	
+	var modal = $("#modalPopup");
+	
+	modal.find('.name-modal').html(name);
+	modal.find('.bio-modal').html('ayyyyyy');
+}
